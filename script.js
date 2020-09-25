74c05b08b03b67e20c4060f00502e0b6f426bd31
= <=>
2020-09-25 16:44:38 +0530

    Hits Update

--- script.js
+++ script.js
@@ -7,29 +7,45 @@ const audio = document.getElementById('audio');
 const progress = document.getElementById('progress');
 const progressContainer = document.getElementById('progress-container');
 const title = document.getElementById('title');
+const movie = document.getElementById('movie');
 const cover = document.getElementById('cover');
 
 
 // Song titles
-const songs = ['18 Vayathil.mp3', 'Aadatha Aatamellam.mp3', 'Adada Mazhaida.mp3', 'Adada Vaa.mp3',
-    'Aedho Saigirai.mp3', 'Arabu Naade.mp3', 'Con Man Theme.mp3', 'Devathayai Kanden.mp3',
-    'Dope Track.mp3', 'Edhirthu Nil.mp3', 'En Anbae En Anbae.mp3', 'En Fuse Pochu.mp3',
-    'En Jannal Vandha.mp3', 'En Kadhal Solla.mp3', 'Engeyo Partha.mp3', 'Eno Kangal.mp3',
-    'High On Love.mp3', 'Idhu Kadhala.mp3', 'Idhu Varai.mp3', 'Iragai Pole.mp3', 'Irava Pagala.mp3',
-    'Kaadal Endral.mp3', 'Kaatrukullae.mp3', 'Kadhal Konden.mp3', 'Kadhal Vaithu.mp3',
-    'Kadhal Valarthen.mp3', 'Kadhal Vandhale.mp3', 'Kambathu Ponnu.mp3', 'Kan Peasum Varthaigal.mp3',
-    'Loosu Penne.mp3', 'Mella Sirithal.mp3', 'Merke Merke.mp3', 'Nahnh Na Nah (New Jack Swing Mix).mp3',
-    'Natpinilae Natpinilae.mp3', 'Natpukullae Oru.mp3', 'Neethane.mp3', 'Nenjodu Kalinthidu.mp3',
-    'Ninaithu Ninaithu Parthal.mp3', 'Oh Shala.mp3', 'Oru Devadha.mp3', 'Oru Kal.mp3',
-    'Oru Naalaikkul.mp3', 'Oru Naalil - It All Comes Down To This.mp3', 'Oru Paarvaiyil.mp3',
-    'Pesa Vanthen.mp3', 'Pogathey Pogathe.mp3', 'Poi Solla Intha Manasukku.mp3',
-    'Poongatre Poongatre.mp3', 'Siragugal.mp3', 'Sol Pechu.mp3', 'Sollamal Thottu Chellum Thendral.mp3',
-    'Stylish Thamizhachi.mp3', 'Suthuthe Suthuthe Bhoomi.mp3', 'Sutta Suriyane.mp3',
-    'Teriyaamaley Toleygiyren.mp3', 'Thaakkuthe.mp3', 'Thathi Thathi Thaavuthey.mp3', 'Theme Music.mp3',
-    'Thottu Thottu.mp3', 'Thuli Thuli Mazhaiyaai.mp3', 'Unnai Thozhi Enbatha.mp3',
-    'Va Va Nilava Pudich.mp3', 'Vaanam.mp3', 'Vandha Kadha.mp3', 'Varriyaa - Night Life.mp3',
-    'Venmegam Pennaga.mp3', 'Vethalaiya Potendi.mp3', 'Walking Through The Rainbow.mp3',
-    'Yaaro (Friendship).mp3', 'Yedhedo Ennangal Vandhu.mp3', 'Yedho Ondru Ennai.mp3'
+const songs = ['18 Vayathil - Kadhal Kondein.mp3', 'Aadatha Aatamellam - Mounam Pesiyathey.mp3',
+    'Adada Mazhaida - Paiyaa.mp3', 'Adada Vaa - Sarvam.mp3', 'Aedho Saigirai - Vaamanan.mp3',
+    'Arabu Naade - Thottal Poo Malarum.mp3', 'Con Man Theme - Masss.mp3',
+    'Devathayai Kanden - Kadhal Kondein.mp3', 'Dope Track - Pyaar Prema Kaadhal.mp3',
+    'Edhirthu Nil - Biriyani.mp3', 'En Anbae En Anbae - Mounam Pesiyathey.mp3',
+    'En Fuse Pochu - Aarambam.mp3', 'En Jannal Vandha - Theeradha Vilayattu Pillai.mp3',
+    'En Kadhal Solla - Paiyaa.mp3', 'Engeyo Partha - Yaaradi Nee Mohini.mp3',
+    'Eno Kangal - Kalvanin Kaadhali.mp3', 'High On Love - Pyaar Prema Kaadhal.mp3',
+    'Idhu Kadhala - Thulluvadho Ilamai.mp3', 'Idhu Varai - Goa.mp3',
+    'Iragai Pole - Naan Mahan Alla.mp3', 'Irava Pagala - Poovellam Kaettu Paar.mp3',
+    'Kaadal Endral - Goa.mp3', 'Kaatrukullae - Sarvam.mp3', 'Kadhal Konden - Kadhal Kondein.mp3',
+    'Kadhal Vaithu - Deepavali.mp3', 'Kadhal Valarthen - Manmadhan.mp3',
+    'Kadhal Vandhale - Vallavan.mp3', 'Kambathu Ponnu - Sandakozhi 2.mp3',
+    'Kan Peasum Varthaigal - 7G Rainbow Colony.mp3', 'Loosu Penne - Vallavan.mp3',
+    'Mella Sirithal - Aadhalal Kadhal Seiveer.mp3', 'Merke Merke - Kanda Naal Mudhalaai.mp3',
+    'Nahnh Na Nah (New Jack Swing Mix) - Biriyani.mp3', 'Natpinilae Natpinilae - Kadhal Kondein.mp3',
+    'Natpukullae Oru - Chennai 600028.mp3', 'Neethane - Sarvam.mp3',
+    'Nenjodu Kalinthidu - Kadhal Kondein.mp3', 'Ninaithu Ninaithu Parthal - 7G Rainbow Colony.mp3',
+    'Oh Shala - Kaadhal Solla Vandhen.mp3', 'Oru Devadhai - Vaamanan.mp3',
+    'Oru Kal - Siva Manasula Sakthi.mp3', 'Oru Naalaikkul - Yaaradi Nee Mohini.mp3',
+    'Oru Naalil - Pudhupettai.mp3', 'Oru Paarvaiyil - Siva Manasula Sakthi.mp3',
+    'Pesa Vanthen - Manmadhan.mp3', 'Pogathey Pogathe - Deepavali.mp3', 'Poi Solla Intha Manasukku - April Maadhathil.mp3',
+    'Poongatre Poongatre - Paiyaa.mp3', 'Siragugal - Sarvam.mp3', 'Sol Pechu - Thillalangadi.mp3',
+    'Sollamal Thottu Chellum Thendral - Dheena.mp3', 'Stylish Thamizhachi - Aarambam.mp3',
+    'Suthuthe Suthuthe Bhoomi - Paiyaa.mp3', 'Sutta Suriyane - Sarvam.mp3',
+    'Teriyaamaley Toleygiyren - Theeradha Vilayattu Pillai.mp3',
+    'Thaakkuthe - Baana Kaathadi.mp3', 'Thathi Thathi Thaavuthey - Kadhal Kondein.mp3',
+    'Theme Music - Sarvam.mp3', 'Thottu Thottu - Kadhal Kondein.mp3',
+    'Thuli Thuli Mazhaiyaai - Paiyaa.mp3', 'Unnai Thozhi Enbatha - Kadhal Kondein.mp3',
+    'Va Va Nilava Pudichu - Naan Mahan Alla.mp3', 'Vaanam - Vaanam.mp3',
+    'Vandha Kadha - Vai Raja Vai.mp3', 'Varriyaa - Pudhupettai.mp3',
+    'Venmegam Pennaga - Yaaradi Nee Mohini.mp3', 'Vethalaiya Potendi - Billa.mp3',
+    'Walking Through The Rainbow - 7G Rainbow Colony.mp3', 'Yaaro (Friendship) - Chennai 600028.mp3',
+    'Yedhedo Ennangal Vandhu - Pattiyal.mp3', 'Yedho Ondru Ennai - Paiyaa.mp3'
 ];
 
 // Keep track of song
@@ -40,9 +56,10 @@ loadSong(songs[songIndex]);
 
 // Update song details
 function loadSong(song) {
-    title.innerText = song.split('.')[0];
+    title.innerText = song.split('.')[0].split(' - ')[0];
+    movie.innerText = song.split('.')[0].split(' - ')[1];
     audio.src = `music/${song}`;
-    cover.src = `music/offset.jpg`;
+    cover.src = `images/${song.split('.')[0].split(' - ')[1]}.jpg`;
 }
 
 // Play song
